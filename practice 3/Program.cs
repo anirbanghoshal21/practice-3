b7c33e39a27f9541693b649cec9fc640d7d0df65
username <username@cognizant.com>
2022-08-19 16:56:20 +0530

    Add project files.

---
+++ practice 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace practice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1, number2, number3, number4;
+
+            Console.Write("Enter the First number: ");
+            number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the Second number: ");
+            number2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the third number: ");
+            number3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the fourth number: ");
+            number4 = Convert.ToDouble(Console.ReadLine());
+
+            double result = (number1 + number2 + number3 + number4) / 4;
+            Console.WriteLine("The average of {0}, {1}, {2}, {3} is: {4} ",
+         number1, number2, number3, number4, result);
+            Console.ReadLine();
+        }
+    }
+}
